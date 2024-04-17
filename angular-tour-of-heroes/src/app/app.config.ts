ea40522307812ae30b62bfec523d334ce1614ea4
hurd1993 <hurdca1993@gmail.com>
2024-04-17 13:41:43 -0400

    Section 6: Finished add search to the dashboard

--- angular-tour-of-heroes/src/app/app.config.ts
+++ angular-tour-of-heroes/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(HttpClientModule),
+  importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation: false}))]
 };
